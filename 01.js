b68783cab131512edb548685efa9437f14dffa22
carinebecker <caarine.m@gmail.com>
2021-11-08 16:27:39 -0300

    fix: 01

--- 01.js
+++ 01.js
@@ -3,23 +3,16 @@
 // b. Obtenha uma array contendo apenas os números
 // c. Obtenha o maior número da array acima
 
-function getStrings(arr) {
+function filterArray(arr) {
     const strings = arr.filter((item) => typeof item === 'string')
-    return strings
-}
-
-function getNumbers(arr) {
-    const numbers = arr.filter((item) => typeof item === 'number')
-    return numbers
-}
-
-function getMaxNumber(arr) {
     const numbers = arr.filter((item) => typeof item === 'number')
     const maxNumber = Math.max(...numbers)
-    return maxNumber
+    return {
+        strings,
+        numbers,
+        maxNumber
+    }
 }
 
 const array = [ 'a', 10, 'b', 'hola', 122, 15]
-console.log('Strings: ', getStrings(array))
-console.log('Numbers: ', getNumbers(array))
-console.log('Max number: ', getMaxNumber(array))
+console.log(filterArray(array))
